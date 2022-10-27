c4bb9ccc28652adacbf1298b72d254c871114c72
Ayo <ayoinspired@gmail.com>
2022-10-27 12:01:02 +0100

    Minor fix

--- src/components/landing/CategoryFilters.tsx
+++ src/components/landing/CategoryFilters.tsx
@@ -12,12 +12,12 @@ const CategoryFilters = ({
 	setSelectedCategory,
 }: Props) => {
 	return (
-		<div className="mb-8 sticky top-14 z-40 pry-bg py-5 w-full flex items-center justify-around">
+		<div className="mb-8 sticky top-14 z-40 pry-bg py-5 w-full flex items-center justify-start md:justify-around overflow-x-scroll scrollbar-hide">
 			{categories?.map((item: string) => (
 				<button
 					key={item}
 					onClick={() => setSelectedCategory(item)}
-					className={`outline-none border capitalize text-sm py-1.5 px-3 rounded-sm transition-colors duration-150 ${
+					className={`outline-none mr-4 md:mr-0 whitespace-nowrap border capitalize text-sm py-1.5 px-3 rounded-sm transition-colors duration-150 ${
 						item === selectedCategory
 							? "bg-white text-[#388e3c] font-medium border-[#388e3c] shadow-lg"
 							: "border-white text-white"
